5f3db0c412424cb18abea41925c6a73746509eb3
Darwin <dhdo@ncsu.edu>
2017-11-08 02:19:24 -0800

    First commit

---
+++ src/actions/secrets.js
@@ -0,0 +1,4 @@
+const username = "harrisonddo";
+const password = "D4tOz@1489";
+
+export { username, password };
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers/index'
+import { fetchMemes } from './actions';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+store.subscribe(() => console.log("store", store.getState()));
+store.dispatch(fetchMemes());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
+  document.getElementById("root")
+)
